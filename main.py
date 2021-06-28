a09a720c4a448cdf5fd41ecdad4f8456ad444857
Mateusz Pasoń <mateuszpason64@gmail.com>
2021-06-28 13:48:15 +0200

    Filename clear out of illegal Windows characters

--- main.py
+++ main.py
@@ -2,20 +2,35 @@ from pytube import YouTube
 from moviepy.editor import *
 import time
 
-user_link = input('Provide video link: ')
-yt = YouTube(user_link)
 
-print('Information about video:')
-print('Author: ', yt.author)
-print('Title: ', yt.title)
-print('Duration: ' + time.strftime('%H:%M:%S', time.gmtime(yt.length)))
+def convert_and_download():
+    user_link = input('Provide video link: ')
+    yt = YouTube(user_link)
 
-to_download = yt.streams.get_highest_resolution()
+    print('Information about video:')
+    print('Author: ', yt.author)
+    print('Title: ', yt.title)
+    print('Duration: ' + time.strftime('%H:%M:%S', time.gmtime(yt.length)))
 
-video_to_convert = VideoFileClip(to_download.download('D:/'))
-video_to_convert.audio.write_audiofile(os.path.join('D:/' + yt.title + '.mp3'))
+    to_download = yt.streams.get_highest_resolution()
 
-# Delete file to convert
-video_to_convert.close()
-path_to_delete_file = 'D:/' + yt.title.replace('.', '') + '.mp4'
-os.remove(path_to_delete_file)
+    video_to_convert = VideoFileClip(to_download.download('D:/'))
+    video_to_convert.audio.write_audiofile(os.path.join('D:/' + delete_illegal_chars(yt.title) + '.mp3'))
+
+    # Delete file to convert
+    video_to_convert.close()
+    path_to_delete_file = 'D:/' + delete_illegal_chars(yt.title) + '.mp4'
+    os.remove(path_to_delete_file)
+
+
+def delete_illegal_chars(filename):
+    illegal_chars = [':', '/', '*', '"', '<', '>', '|', '.']
+    for i in illegal_chars:
+        filename = filename.replace(i, '')
+    return filename
+
+
+try:
+    convert_and_download()
+except:
+    print('Invalid link')
