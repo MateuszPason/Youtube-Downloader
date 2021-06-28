7835223d0f02750cea4489b628ad9bdcedf270eb
Mateusz Pasoń <mateuszpason64@gmail.com>
2021-06-29 00:28:47 +0200

    Function split (download and convert)

--- main.py
+++ main.py
@@ -1,36 +1,64 @@
 from pytube import YouTube
 from moviepy.editor import *
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QDialog
+from PyQt5.uic import loadUi
 import time
 
 
-def convert_and_download():
-    user_link = input('Provide video link: ')
-    yt = YouTube(user_link)
-
-    print('Information about video:')
-    print('Author: ', yt.author)
-    print('Title: ', yt.title)
-    print('Duration: ' + time.strftime('%H:%M:%S', time.gmtime(yt.length)))
-
-    to_download = yt.streams.get_highest_resolution()
-
-    video_to_convert = VideoFileClip(to_download.download('D:/'))
-    video_to_convert.audio.write_audiofile(os.path.join('D:/' + delete_illegal_chars(yt.title) + '.mp3'))
-
-    # Delete file to convert
-    video_to_convert.close()
-    path_to_delete_file = 'D:/' + delete_illegal_chars(yt.title) + '.mp4'
-    os.remove(path_to_delete_file)
-
-
 def delete_illegal_chars(filename):
-    illegal_chars = [':', '/', '*', '"', '<', '>', '|', '.']
+    illegal_chars = [':', '/', '*', '"', '<', '>', '|', '.', '\'']
     for i in illegal_chars:
         filename = filename.replace(i, '')
     return filename
 
 
+class DownloadingComponent(QDialog):
+    def __init__(self):
+        super(DownloadingComponent, self).__init__()
+        self.ui = loadUi("interface/mainWindow.ui", self)
+        self.pushButton.clicked.connect(self.choose_dir_and_download)
+
+    def choose_dir_and_download(self):
+        try:
+            selected_format = self.format.currentText()
+            user_link = self.source.text()
+            yt = YouTube(user_link)
+            folder_path = QtWidgets.QFileDialog.getExistingDirectory(self, 'Select Folder')
+            if selected_format == 'MP3':
+                self.download(folder_path, yt)
+                self.convert(folder_path, yt)
+            else:
+                self.download(folder_path, yt)
+        except:
+            print('invalid link')
+
+    def download(self, folder_path, yt):
+        # print('Duration: ' + time.strftime('%H:%M:%S', time.gmtime(yt.length)))
+
+        to_download = yt.streams.get_highest_resolution()
+        video_to_convert = VideoFileClip(to_download.download(folder_path + '/'))
+
+    def convert(self, folder_path, yt):
+        video_to_convert = VideoFileClip(folder_path + '/' + delete_illegal_chars(yt.title) + '.mp4')
+        video_to_convert.audio.write_audiofile(os.path.join(folder_path + '/' + delete_illegal_chars(yt.title) + '.mp3'))
+        # Delete video after conversion
+        video_to_convert.close()
+        path_to_delete_file = folder_path + '/' + delete_illegal_chars(yt.title) + '.mp4'
+        os.remove(path_to_delete_file)
+
+def main():
+    app = QApplication(sys.argv)
+    main_window = DownloadingComponent()
+    main_window.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
+"""
 try:
     convert_and_download()
 except:
     print('Invalid link')
+"""
